92f8163d154caa60b862abde4993679779f4ea54
Mauro Chojrin <mchojrin@gmail.com>
2018-08-29 20:00:48 -0300

    Access to Doctrine EntityManager ala Symfony4

--- src/Command/CopyFixedExpensesCommand.php
+++ src/Command/CopyFixedExpensesCommand.php
@@ -13,42 +13,48 @@ use App\Entity\GastoFijo;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Doctrine\ORM\EntityManagerInterface;
 
 class CopyFixedExpensesCommand extends ContainerAwareCommand
 {
+    private $em;
+
+    public function __construct(?string $name = null, EntityManagerInterface $em)
+    {
+        parent::__construct($name);
+
+        $this->em = $em;
+    }
+
     protected function configure()
     {
         $this
             // the name of the command (the part after "bin/console")
             ->setName('app:copy-fixed-expenses')
-
             // the short description shown while running "php bin/console list"
             ->setDescription('Creates new concrete instances of fixed expenses for the upcoming month.')
-
             // the full command description shown when running the command with
             // the "--help" option
-            ->setHelp('This command creates new expenses based on the fixed expenses templates')
-        ;
+            ->setHelp('This command creates new expenses based on the fixed expenses templates');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $doctrine = $this->getContainer()->get('doctrine');
-        $entityManager = $doctrine->getEntityManager();
-        $firstDay = new \DateTimeImmutable( 'First day of next month' );
+        $em = $this->em;
+        $firstDay = new \DateTimeImmutable('First day of next month');
 
-        foreach ( $doctrine->getRepository('App:GastoFijo')->findAll() as $gastoFijo ) {
+        foreach ($em->getRepository('App:GastoFijo')->findAll() as $gastoFijo) {
             $fecha = $firstDay->add(new \DateInterval('P' . $gastoFijo->getDia() . 'D'));
-            $output->writeln('Creando gasto de "'.$gastoFijo->getConcepto().'", dia '.$fecha->format('d/m/Y'));
+            $output->writeln('Creando gasto de "' . $gastoFijo->getConcepto() . '", dia ' . $fecha->format('d/m/Y'));
             $debito = new Movimiento();
             $debito->setClonDe($gastoFijo);
             $debito->setFecha($fecha);
-            $debito->setConcepto( $gastoFijo->getConcepto() );
-            $debito->setImporte( $gastoFijo->getImporte() * -1 );
-            $debito->setBanco( $gastoFijo->getBanco() );
-            $entityManager->persist( $debito );
+            $debito->setConcepto($gastoFijo->getConcepto());
+            $debito->setImporte($gastoFijo->getImporte() * -1);
+            $debito->setBanco($gastoFijo->getBanco());
+            $em->persist($debito);
         }
 
-        $entityManager->flush();
+        $em->flush();
     }
 }
