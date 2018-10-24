efb7b4af23dc851922964348a2fd2c17d48442ea
Mauro Chojrin <mchojrin@gmail.com>
2018-10-23 22:14:26 -0300

    Check for known xls structure to allow for bank summary uploading and processing

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -171,54 +171,58 @@ class AdminController extends BaseAdminController
         $id = $request->query->get('id');
         $banco = $repository->find($id);
 
-        $form = $this
-            ->createFormBuilder( null, [ 'data_class' => null ] )
-            ->setAttribute('class', 'form-horizontal new-form')
-            ->add(
-                'File',
-                FileType::class,
-                [
-                    'label' => 'Archivo',
-                    'required' => true,
-                ]
-            )
-            ->add('Subir', SubmitType::class)
-            ->getForm();
+        if ( $xlsStructure = $banco->getXLSStructure() ) {
+            $form = $this
+                ->createFormBuilder( null, [ 'data_class' => null ] )
+                ->setAttribute('class', 'form-horizontal new-form')
+                ->add(
+                    'File',
+                    FileType::class,
+                    [
+                        'label' => 'Archivo',
+                        'required' => true,
+                    ]
+                )
+                ->add('Subir', SubmitType::class)
+                ->getForm();
 
-        $form->handleRequest($request);
-        if ( $form->isSubmitted() && $form->isValid() ) {
-            /** @todo Procesar upload */
-            $file = $form['File']->getData();
-            $spreadsheet = IOFactory::load( $file );
+            $form->handleRequest($request);
+            if ( $form->isSubmitted() && $form->isValid() ) {
+                /** @todo Procesar upload */
+                $file = $form['File']->getData();
+                $spreadsheet = IOFactory::load( $file );
 
-            $xlsStructure = $banco->getXLSStructure();
-            $shit = $spreadsheet->getActiveSheet();
-            $row = $xlsStructure->getFirstRow();
-            $dateContents = $shit->getCellByColumnAndRow( $xlsStructure->getDateCol(), $row )->getValue();
-            while ( ( !empty( $xlsStructure->getStopWord() ) && substr( $dateContents, 0, strlen( $xlsStructure->getStopWord() ) ) !== $xlsStructure->getStopWord() ) || ( empty($xlsStructure->getStopWord() ) && !empty( $dateContents ) ) ) {
-                $date = \DateTime::createFromFormat( $xlsStructure->getDateFormat(), $dateContents );
-                $amount = $shit->getCellByColumnAndRow( $xlsStructure->getAmountCol(), $row )->getValue();
-                $concept = $shit->getCellByColumnAndRow( $xlsStructure->getConceptCol(), $row )->getValue();
-                $transaction = new Movimiento();
-                $transaction->setBanco( $banco );
-                $transaction->setImporte( $amount );
-                $transaction->setFecha( $date );
-                $transaction->setConcepto( $concept );
-
-                $em->persist( $transaction );
-                $row++;
+                $xlsStructure = $banco->getXLSStructure();
+                $shit = $spreadsheet->getActiveSheet();
+                $row = $xlsStructure->getFirstRow();
                 $dateContents = $shit->getCellByColumnAndRow( $xlsStructure->getDateCol(), $row )->getValue();
+                while ( ( !empty( $xlsStructure->getStopWord() ) && substr( $dateContents, 0, strlen( $xlsStructure->getStopWord() ) ) !== $xlsStructure->getStopWord() ) || ( empty($xlsStructure->getStopWord() ) && !empty( $dateContents ) ) ) {
+                    $date = \DateTime::createFromFormat( $xlsStructure->getDateFormat(), $dateContents );
+                    $amount = $shit->getCellByColumnAndRow( $xlsStructure->getAmountCol(), $row )->getValue();
+                    $concept = $shit->getCellByColumnAndRow( $xlsStructure->getConceptCol(), $row )->getValue();
+                    $transaction = new Movimiento();
+                    $transaction->setBanco( $banco );
+                    $transaction->setImporte( $amount );
+                    $transaction->setFecha( $date );
+                    $transaction->setConcepto( $concept );
+
+                    $em->persist( $transaction );
+                    $row++;
+                    $dateContents = $shit->getCellByColumnAndRow( $xlsStructure->getDateCol(), $row )->getValue();
+                }
+
+                $em->flush();
             }
 
-            $em->flush();
+            return $this->render(
+                'admin/cargar_extracto.html.twig',
+                [
+                    'form' => $form->createView(),
+                    'banco' => $banco->getNombre(),
+                ]
+            );
         }
 
-        return $this->render(
-            'admin/cargar_extracto.html.twig',
-            [
-                'form' => $form->createView(),
-                'banco' => $banco->getNombre(),
-            ]
-        );
+        return $this->redirect('/');
     }
 }
