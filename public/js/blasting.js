9585a4392ff1e49d68900a2e2d80276612a4551e
Mauro Chojrin <mchojrin@gmail.com>
2018-11-10 16:16:15 -0300

    Disallowing 0 amount transactions

--- public/js/blasting.js
+++ public/js/blasting.js
@@ -1,9 +1,17 @@
 $('.action-save').click(function(){
     if ( $('#debito_importe').length ) {
+        if ( $('#debito_importe').val() == 0 ) {
+
+            return false;
+        }
         $('#debito_importe').val( Math.abs($('#debito_importe').val() ) * - 1 );
     }
 
     if ( $('#credito_importe').length ) {
+        if ( $('#credito_importe').val() == 0 ) {
+
+            return false;
+        }
         $('#credito_importe').val( Math.abs($('#credito_importe').val() ) );
     }
 });
