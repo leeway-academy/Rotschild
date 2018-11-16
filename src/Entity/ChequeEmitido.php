ffd2a7c2d1d728b0dd43d79930c178005b5bc501
Mauro Chojrin <mchojrin@gmail.com>
2018-11-16 16:09:34 -0300

    Added support for issued checks storing

---
+++ src/Entity/ChequeEmitido.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ChequeEmitidoRepository")
+ */
+class ChequeEmitido
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Banco", inversedBy="chequesEmitidos")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $banco;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $numero;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $fecha;
+
+    /**
+     * @ORM\Column(type="decimal", precision=10, scale=2)
+     */
+    private $importe;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getBanco(): ?Banco
+    {
+        return $this->banco;
+    }
+
+    public function setBanco(?Banco $banco): self
+    {
+        $this->banco = $banco;
+
+        return $this;
+    }
+
+    public function getNumero(): ?string
+    {
+        return $this->numero;
+    }
+
+    public function setNumero(string $numero): self
+    {
+        $this->numero = $numero;
+
+        return $this;
+    }
+
+    public function getFecha(): ?\DateTimeInterface
+    {
+        return $this->fecha;
+    }
+
+    public function setFecha(\DateTimeInterface $fecha): self
+    {
+        $this->fecha = $fecha;
+
+        return $this;
+    }
+
+    public function getImporte()
+    {
+        return $this->importe;
+    }
+
+    public function setImporte($importe): self
+    {
+        $this->importe = $importe;
+
+        return $this;
+    }
+}
