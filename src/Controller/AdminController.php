1e75cbbc6c6c22d8bd70956f7721afed650d113d
Mauro Chojrin <mchojrin@gmail.com>
2018-12-05 19:03:00 -0300

    Custom action to handle Ajax

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -944,4 +944,51 @@ class AdminController extends BaseAdminController
             ]
         );
     }
+
+    protected function newDebitoAction()
+    {
+        $this->dispatch(EasyAdminEvents::PRE_NEW);
+
+        $entity = $this->executeDynamicMethod('createNew<EntityName>Entity');
+
+        $easyadmin = $this->request->attributes->get('easyadmin');
+        $easyadmin['item'] = $entity;
+        $this->request->attributes->set('easyadmin', $easyadmin);
+
+        $fields = $this->entity['new']['fields'];
+
+        $newForm = $this->executeDynamicMethod('create<EntityName>NewForm', array($entity, $fields));
+
+        $newForm->handleRequest($this->request);
+        if ($newForm->isSubmitted() && $newForm->isValid()) {
+            $this->dispatch(EasyAdminEvents::PRE_PERSIST, array('entity' => $entity));
+
+            $this->executeDynamicMethod('prePersist<EntityName>Entity', array($entity, true));
+            $this->executeDynamicMethod('persist<EntityName>Entity', array($entity, $newForm));
+
+            $this->dispatch(EasyAdminEvents::POST_PERSIST, array('entity' => $entity));
+
+            if ( !$this->request->isXmlHttpRequest() ) {
+
+                return $this->redirectToReferrer();
+            } else {
+
+                return new JsonResponse();
+            }
+        }
+
+        $this->dispatch(EasyAdminEvents::POST_NEW, array(
+            'entity_fields' => $fields,
+            'form' => $newForm,
+            'entity' => $entity,
+        ));
+
+        $parameters = array(
+            'form' => $newForm->createView(),
+            'entity_fields' => $fields,
+            'entity' => $entity,
+        );
+
+        return $this->executeDynamicMethod('render<EntityName>Template', array('new', $this->entity['templates']['new'], $parameters));
+    }
 }
