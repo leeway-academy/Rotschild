c38c4d37ff2bac0068810a5b6502fddeecbf815e
Mauro Chojrin <mchojrin@gmail.com>
2018-12-06 11:30:06 -0300

    Initial implementation of inline debit edition

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -1052,4 +1052,66 @@ class AdminController extends BaseAdminController
 
         return $this->executeDynamicMethod('render<EntityName>Template', array('new', $this->entity['templates']['new'], $parameters));
     }
+
+    protected function editDebitoAction()
+    {
+        $this->dispatch(EasyAdminEvents::PRE_EDIT);
+
+        $id = $this->request->query->get('id');
+        $easyadmin = $this->request->attributes->get('easyadmin');
+        $entity = $easyadmin['item'];
+
+        if ($this->request->isXmlHttpRequest() && $property = $this->request->query->get('property')) {
+            $newValue = 'true' === mb_strtolower($this->request->query->get('newValue'));
+            $fieldsMetadata = $this->entity['list']['fields'];
+
+            if (!isset($fieldsMetadata[$property]) || 'toggle' !== $fieldsMetadata[$property]['dataType']) {
+                throw new \RuntimeException(sprintf('The type of the "%s" property is not "toggle".', $property));
+            }
+
+            $this->updateEntityProperty($entity, $property, $newValue);
+
+            // cast to integer instead of string to avoid sending empty responses for 'false'
+            return new Response((int) $newValue);
+        }
+
+        $fields = $this->entity['edit']['fields'];
+
+        $editForm = $this->executeDynamicMethod('create<EntityName>EditForm', array($entity, $fields));
+        $deleteForm = $this->createDeleteForm($this->entity['name'], $id);
+
+        $editForm->handleRequest($this->request);
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->dispatch(EasyAdminEvents::PRE_UPDATE, array('entity' => $entity));
+
+            $this->executeDynamicMethod('preUpdate<EntityName>Entity', array($entity, true));
+            $this->executeDynamicMethod('update<EntityName>Entity', array($entity, $editForm));
+
+            $this->dispatch(EasyAdminEvents::POST_UPDATE, array('entity' => $entity));
+
+            if ( !$this->request->isXmlHttpRequest() ) {
+
+                return $this->redirectToReferrer();
+            } else {
+
+                return new JsonResponse(
+                    [
+                        'id' => $entity->getId(),
+                        'string' => $entity->__toString(),
+                    ]
+                );
+            }
+        }
+
+        $this->dispatch(EasyAdminEvents::POST_EDIT);
+
+        $parameters = array(
+            'form' => $editForm->createView(),
+            'entity_fields' => $fields,
+            'entity' => $entity,
+            'delete_form' => $deleteForm->createView(),
+        );
+
+        return $this->executeDynamicMethod('render<EntityName>Template', array('edit', $this->entity['templates']['edit'], $parameters));
+    }
 }
