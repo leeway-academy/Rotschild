2ad6724843eb8eaef47416c0804a2d2d8ebf62a1
Mauro Chojrin <mchojrin@gmail.com>
2018-12-12 11:39:16 -0300

    Sql script to restore database to a pristine state

---
+++ reset_db.sql
@@ -0,0 +1,16 @@
+delete
+from applied_check;
+
+delete from cheque_emitido;
+
+delete from extracto_bancario;
+
+delete from gasto_fijo;
+
+delete from movimiento;
+
+delete from renglon_extracto;
+
+delete from saldo_bancario;
+
+
