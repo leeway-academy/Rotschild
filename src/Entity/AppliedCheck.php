2806808d19ed5e18bf153451ef635a618129ff02
Mauro Chojrin <mchojrin@gmail.com>
2018-12-28 12:48:51 -0300

    Applied check is one possible way to mark a transaction as concreted

--- src/Entity/AppliedCheck.php
+++ src/Entity/AppliedCheck.php
@@ -7,7 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Entity(repositoryClass="App\Repository\AppliedCheckRepository")
  */
-class AppliedCheck
+class AppliedCheck implements Witness
 {
     /**
      * @ORM\Id()
