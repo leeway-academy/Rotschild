e7db6ce33f4b53051bf19c439850cb89f5bcf457
Mauro Chojrin <mchojrin@gmail.com>
2018-12-28 13:47:38 -0300

    Moving applied checks logic into its own controller

---
+++ src/Controller/AppliedCheckController.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mauro
+ * Date: 12/28/18
+ * Time: 1:31 PM
+ */
+
+namespace App\Controller;
+
+use App\Entity\Movimiento;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AdminController as BaseAdminController;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AppliedCheckController extends BaseAdminController
+{
+    /**
+     * @param Request $request
+     * @Route(path="/processedAppliedChecks", name="process_applied_checks")
+     */
+
+    public function process(Request $request)
+    {
+        /*
+         * @todo This could be much more intelligent since the destination of the check is part of the
+         * Excel file...
+         */
+        $formBuilder = $this->createFormBuilder();
+
+        $bancos = $this->getDoctrine()->getRepository('App:Bank')->findAll();
+        $debits = $this->getDoctrine()->getRepository('App:Movimiento')->findProjectedCredits();
+        $checks = $this->getDoctrine()->getRepository('App:AppliedCheck')->findAll();
+
+        foreach ($checks as $k => $check) {
+            if ( $check->isMatched() ) {
+                unset( $checks[$k] );
+
+                continue;
+            }
+            $formBuilder->add(
+                'bank_' . $k,
+                ChoiceType::class,
+                [
+                    'choices' => $bancos,
+                    'choice_label' => function ($choiceValue, $key, $value) {
+
+                        return $choiceValue . '';
+                    },
+                    'choice_value' => 'id',
+                    'required' => false,
+                ]
+            );
+            $formBuilder->add(
+                'debit_' . $k,
+                ChoiceType::class,
+                [
+                    'choices' => $debits,
+                    'choice_label' => function ($choiceValue, $key, $value) {
+
+                        return $choiceValue . '';
+                    },
+                    'choice_value' => 'id',
+                    'required' => false,
+                ]
+            );
+        }
+
+        $formBuilder->add(
+            'apply',
+            SubmitType::class,
+            [
+                'attr' => [
+                    'class' => 'btn btn-primary',
+                ]
+            ]
+        );
+
+        $form = $formBuilder->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            foreach ($checks as $k => $check) {
+                $movimiento = $form['debit_' . $k]->getData();
+                $recipientBank = $form['bank_' . $k]->getData();
+                if (!empty($recipientBank) || !empty($movimiento)) {
+                    if (!empty($recipientBank)) {
+                        $movimiento = new Movimiento();
+                        $movimiento
+                            ->setBank($recipientBank)
+                            ->setImporte($check->getAmount())
+                            ->setFecha($check->getCreditDate())
+                            ->setConcepto('Acreditacion de cheque ' . $check->getNumber())
+                        ;
+
+                        $check->setChildCredit($movimiento);
+                        $em->persist($movimiento);
+                        $em->persist($check);
+                    } elseif (!empty($movimiento)) {
+                        /**
+                         * WTF ???
+                         * The transaction must not subtract from upcoming balances since the debt has been payed
+                         * Should it be deleted though? How to take it back if the matching was incorrect?
+                         */
+                    }
+                }
+            }
+
+            $em->flush();
+
+            return $this->redirectToRoute('process_applied_checks');
+        }
+
+        return $this->render(
+            'admin/process_applied_checks.html.twig',
+            [
+                'form' => $form->createView(),
+                'checks' => $checks,
+            ]
+        );
+    }
+}
