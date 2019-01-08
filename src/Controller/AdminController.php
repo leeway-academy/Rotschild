4fa707cb47615188014ed455b714ca437059fda3
Mauro Chojrin <mchojrin@gmail.com>
2019-01-08 10:35:01 -0300

    Refactored IssuedChecks into its own controller class

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Entity\AppliedCheck;
 use App\Entity\Bank;
-use App\Entity\ChequeEmitido;
 use App\Entity\ExtractoBancario;
 use App\Entity\Movimiento;
 use App\Entity\RenglonExtracto;
@@ -237,78 +236,6 @@ class AdminController extends BaseAdminController
 
     /**
      * @param Request $request
-     * @Route(name="import_issued_checks", path="/import/issuedChecks")
-     */
-    public function importIssuedChecks(Request $request)
-    {
-        $formBuilder = $this->createFormBuilder()
-            ->setAttribute('class', 'form-vertical new-form');
-
-        $formBuilder->add(
-            'reportFile',
-            FileType::class,
-            [
-                'label' => 'Informe de cheques emitidos ',
-                'required' => true,
-            ]
-        )->add(
-            'Import',
-            SubmitType::class,
-            [
-                'attr' => [
-                    'class' => 'btn btn-primary action-save',
-                ],
-                'label' => 'Importar',
-            ]
-        )->getForm();
-
-        $form = $formBuilder->getForm();
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-
-            foreach ($form->getData() as $name => $item) {
-                if (!is_null($item) && $item->getType() == 'file' && in_array($item->getMimeType(), ['application/wps-office.xls', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.ms-excel'])) {
-                    $fileName = 'IssuedChecks_' . (new \DateTimeImmutable())->format('d-m-y') . '.' . $item->guessExtension();
-                    $item->move($this->getParameter('reports_path'), $fileName);
-
-                    $lines = $this->getExcelReportProcessor()->getIssuedChecks(
-                        IOFactory::load($this->getParameter('reports_path') . DIRECTORY_SEPARATOR . $fileName)
-                    );
-
-                    foreach ($lines as $k => $line) {
-                        $chequeEmitido = new ChequeEmitido();
-                        $chequeEmitido
-                            ->setImporte($line['amount'])
-                            ->setFecha($line['date'])
-                            ->setBanco($em->getRepository('App:Bank')->findOneBy(['codigo' => $line['bankCode']]))
-                            ->setNumero($line['checkNumber']);
-                        $em->persist($chequeEmitido);
-                    }
-
-                    $em->flush();
-                }
-            }
-
-            $this->addFlash(
-                'notice',
-                'Cheques emitidos importados'
-            );
-        }
-
-        return $this->render(
-            'admin/import_excel_reports.html.twig',
-            [
-                'form' => $form->createView(),
-                'reportName' => 'Cheques emitidos',
-            ]
-        );
-    }
-
-    /**
-     * @param Request $request
      * @Route(name="import_applied_checks", path="/import/appliedChecks")
      */
     public function importAppliedChecks(Request $request)
@@ -477,115 +404,6 @@ class AdminController extends BaseAdminController
                 'bank' => $bank,
                 'dateFrom' => $dateFrom,
                 'dateTo' => $dateTo,
-            ]
-        );
-    }
-
-    /**
-     * @param Request $request
-     * @Route(path="/checks/issued/process", name="process_issued_checks")
-     */
-    public function processIssuedChecks(Request $request)
-    {
-        $formBuilder = $this->createFormBuilder();
-
-        $managerRegistry = $this->getDoctrine();
-
-        $debits = $managerRegistry
-            ->getRepository('App:Movimiento')
-            ->findNonCheckProjectedDebits();
-
-        $issuedChecks = $managerRegistry
-            ->getRepository('App:ChequeEmitido')
-            ->findAll();
-
-        $nullOption = [ '-1' => 'Payment to providers' ];
-
-        foreach ($issuedChecks as $k => $check) {
-            if ( $check->getChildDebit() ) {
-                unset( $issuedChecks[$k] );
-
-                continue;
-            }
-            $formBuilder->add(
-                'match_' . $check->getId(),
-                ChoiceType::class,
-                [
-                    'choices' => array_merge( $nullOption, $debits->toArray() ),
-                    'choice_value' => function( $o ) use ( $nullOption ) {
-                        if ( $o instanceof Movimiento ) {
-
-                            return $o->getId();
-                        } elseif ( $o == $nullOption[ '-1' ] ) {
-
-                            return "-1";
-                        } else {
-
-                            return "";
-                        }
-                    },
-                    'choice_label' => function ($d) {
-
-                        return $d . '';
-                    },
-                    'required' => false,
-                    'attr' => [
-                        'class' => 'selectTx',
-                    ],
-                ]
-            );
-        }
-
-        $formBuilder
-            ->add(
-                'submit',
-                SubmitType::class,
-                [
-                    'label' => 'Confirmar',
-                    'attr' => [
-                        'class' => 'btn btn-primary',
-                    ]
-                ]
-            );
-
-        $form = $formBuilder->getForm();
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $objectManager = $managerRegistry->getManager();
-            foreach ($form->getData() as $k => $datum) {
-                $parts = preg_split('/_/', $k);
-                $k = $parts[1];
-                $check = $issuedChecks[$k];
-                if ($datum) {
-                    /**
-                     * A new debit is created for the check itself
-                     */
-                    $checkDebit = $check->createChildDebit();
-                    $objectManager->persist( $checkDebit );
-
-                    if ( $datum instanceof Movimiento ) {
-                        /**
-                         * If it's an existing transaction this check marks it as payed
-                         */
-                        $datum->setWitness( $check );
-                        $objectManager->persist( $datum );
-                    }
-
-                    $objectManager->persist($check);
-                }
-
-                $objectManager->flush();
-            }
-
-            return $this->redirectToRoute('process_issued_checks');
-        }
-
-        return $this->render(
-            'admin/process_issued_checks.html.twig',
-            [
-                'form' => $form->createView(),
-                'checks' => $issuedChecks,
             ]
         );
     }
