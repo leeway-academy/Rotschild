d55cfc0731845e238165f1adcddf59b90ce34876
Mauro Chojrin <mchojrin@gmail.com>
2019-01-21 16:12:39 -0300

    New Twig extension to handle custom actions for debits/credits

---
+++ src/Twig/EasyAdminExtension.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mauro
+ * Date: 1/21/19
+ * Time: 3:31 PM
+ */
+
+namespace App\Twig;
+
+use App\Entity\Movimiento;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class EasyAdminExtension extends AbstractExtension
+{
+    public function getFilters()
+    {
+        return [
+                new TwigFilter( 'filter_admin_actions', [ $this, 'filterActions'] ),
+            ];
+    }
+
+    public function filterActions( array $itemActions, $item )
+    {
+        if ( $item instanceof Movimiento ) {
+            if ( $item->isDebit() ) {
+                if ( $item->isConcretado() ) {
+                    unset($itemActions['delete']);
+                    unset($itemActions['edit']);
+                    $itemActions['undo'] = [
+                        'name' => 'undoDebit',
+                        'type' => 'method',
+                        'label' => 'action.undo',
+                        'title' => null,
+                        'css_class' => 'text-danger',
+                        'icon' => null,
+                        'target' => '_self',
+                    ];
+                }
+            } elseif ( $item->isCredit() ) {
+
+            }
+        }
+
+        return $itemActions;
+    }
+}
