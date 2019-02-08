e2e2da777e500c37f85c2870424e5d0bc8f4ec61
Mauro Chojrin <mchojrin@gmail.com>
2019-02-08 15:40:16 -0300

    Added sql to init bank information

--- reset_db.sql
+++ reset_db.sql
@@ -11,3 +11,13 @@ delete from movimiento;
 delete from renglon_extracto;
 
 delete from saldo_bancario;
+
+delete from bank;
+
+INSERT INTO "bank" VALUES(1,'Banco Río',72);
+INSERT INTO "bank" VALUES(2,'Banco Nación',11);
+
+delete from bank_xlsstructure;
+
+INSERT INTO "bank_xlsstructure" VALUES(1,1,NULL,NULL,NULL,21,6,7,1,NULL);
+INSERT INTO "bank_xlsstructure" VALUES(2,2,NULL,NULL,NULL,13,1,5,2,'d/m/Y');
