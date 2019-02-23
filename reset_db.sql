e196baddebc4244f648ed7a1bc7796f56756b3c2
Mauro Chojrin <mchojrin@gmail.com>
2019-02-23 11:39:28 -0300

    Fixed skipping rows error

--- reset_db.sql
+++ reset_db.sql
@@ -20,4 +20,4 @@ INSERT INTO "bank" VALUES(2,'Banco Nación',11);
 delete from bank_xlsstructure;
 
 INSERT INTO bank_xlsstructure (id, banco_id, stop_word, first_header, extra_data_col, first_row, concept_col, amount_col, date_col, date_format) VALUES (1, 1, null, 'Últimos Movimientos', null, 5, 6, 7, 1, 'm/d/Y');
-INSERT INTO bank_xlsstructure (id, banco_id, stop_word, first_header, extra_data_col, first_row, concept_col, amount_col, date_col, date_format) VALUES (2, 2, null, null, null, 16, 1, 5, 2, 'd/m/Y');
+INSERT INTO bank_xlsstructure (id, banco_id, stop_word, first_header, extra_data_col, first_row, concept_col, amount_col, date_col, date_format) VALUES (2, 2, null, null, null, 15, 1, 5, 2, 'd/m/Y');
